5781b01293779fe899448f9e57dfd27001cf4085
millethescobar <165673662+millethescobar@users.noreply.github.com>
2024-11-12 15:45:38 -0500

    completed

---
+++ surag4-main/src/main/java/com/example/APPY_REST_G4/Servicios/MedicoServicio.java
@@ -0,0 +1,17 @@
+package com.example.APPY_REST_G4.Servicios;
+
+import com.example.APPY_REST_G4.Repositorio.IRepositorioMedico;
+import com.example.APPY_REST_G4.modelos.Medico;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MedicoServicio {
+
+    @Autowired
+    IRepositorioMedico iRepositorioMedico;
+
+    public Medico registrarMedico(Medico datosMedicos){
+        return null;
+    }
+}
